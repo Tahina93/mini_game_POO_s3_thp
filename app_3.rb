e922f82f1753dcdc6a75ed591f9100b2657afb4b
Tahina93 <anne.tahina.metz@gmail.com>
2019-04-16 17:53:04 +0200

    manque dernière partie 3.0

--- app_3.rb
+++ app_3.rb
@@ -5,5 +5,48 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 require_relative 'lib/game'
 
+puts " =========================================="
+puts "| Are you affrayed of Death ??             |"
+puts "| Welcome to the deadliest game !!         |"
+puts "| The goal is simply to STAY ALIVE ...     |"
+puts " =========================================="
+puts "\n "
 
-binding.pry
+  def start
+   #création de l'HumanPlayer (interaction avec l'utilisateur)
+   puts "What's your name ?"
+   print ">"
+   @name = gets.chomp
+   @new_game = Game.new(@name)
+   puts "\n"
+   #Pour afficher que les noms des deux ennemies. et pas leur ID de Player.new.
+   ennemies_name = @new_game.ennemies.map { |ennemie| ennemie.name  } 
+   puts "Tu vas affronter #{ennemies_name.join(" et ")} !"
+
+   print "\n Tap ENTER to continue\n"
+   gets 
+  end
+
+  def fight
+    while @new_game.is_still_ongoing?
+      puts "------------------------------------------"
+  	  puts "Voici ton état : \n"
+  	  @new_game.show_player
+  	  @new_game.menu
+  	  @new_game.menu_choice
+  	  puts "\n Tap ENTER to continue"
+  	  gets
+  	  @new_game.ennemies_attack
+  	  
+  	end
+  	
+  	@new_game.end
+  end
+
+
+
+
+
+start
+fight
+#binding.pry
--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,11 @@
 class Game
 
-attr_accessor :human_player :ennemies
+attr_accessor :human_player, :ennemies
 
   def initialize(name)
     @human_player = HumanPlayer.new(name)
     @ennemies = []
-    ["Machin", "Truc", "Bidule", "Muche"]each { |ennemy| @ennemies << Player.new(ennemy)  }
+    ["Machin", "Truc", "Bidule", "Muche"].each { |ennemy| @ennemies << Player.new(ennemy)  }
   end
 
 
@@ -29,7 +29,67 @@ attr_accessor :human_player :ennemies
     puts "\n Quelle action veux-tu effectuer ?"
     puts "\n\n a - chercher une meilleure arme "
     puts "\n s - chercher de quoi te soigner "
-    puts "\n\n attaquer un adversaire : \n"
-    puts " 0 - #{player1.show_state}  \n 1 - #{player2.show_state} "
+    puts "\n\n attaquer un adversaire :"
+    @ennemies.length.times do |index|
+      puts "#{index} - #{ennemies[index].show_state}"
+    end
+
+
 
   end
+
+  def menu_choice
+
+  	print ">"
+    choice = gets.chomp 
+    #appelle la methode pour recupérer des armes
+  	if choice == "a"
+  	  human_player.search_weapon 	
+  	#appelle la methode pour récupérer des vies
+  	elsif choice == "s"
+  	  human_player.search_health_pack
+
+  	elsif choice.to_i
+  	  human_player.attack(@ennemies[choice.to_i])
+  	    if @ennemies[choice.to_i].life_points <= 0
+  	      kill_player(@ennemies[choice.to_i])
+  	    end 
+    end
+  end
+
+
+
+  def ennemies_attack
+    @ennemies.each do |player|
+      #Si jamais un des joueur meurt, qu'il ne puisse plus attaquer
+      if player.life_points <= 0 
+      	puts "#{player.name} est mort il ne peux plus t'attaquer \n \n"
+    	next #-----> si un seul joueu est mort, l'autre attaque toujours (different de "break" qui sort carrement de la boucle et ça foire si l'un des joueur n'est pas mort)
+      #si pas mort, il(s) attaquent
+      else 
+        player.attack(human_player)
+        puts "\n"
+        print "\n Tap ENTER to continue \n"
+        gets
+      end
+       
+    end
+  end
+
+
+  def end
+    puts "\n The game is over.."
+
+    if human_player.life_points > 0
+  	  puts "Bravo ! You smashed the other dumbass. You made them your bitches "
+    else 
+  	  puts "Bouuuuuuuh You suck balls and got fucked by those basic bitches !"
+    end
+  end
+
+end
+
+
+
+
+
