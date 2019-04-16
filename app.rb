8e2b049361e9532f787b92dbb69982d77335fe4d
Tahina93 <anne.tahina.metz@gmail.com>
2019-04-16 11:59:22 +0200

    first commit game 1.0 gone

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'bundler'
+Bundler.require
+
+require_relative 'lib/game'
+require_relative 'lib/player'
+
+player1 = Player.new("Gus")
+player2 = Player.new("Xi-xi")
+
+puts "#{player1.name} et #{player2.name} vont s'affronter sans pitié !!!"
+
+while player1.life_points > 0 && player2.life_points >0 
+  puts "-----------------------------------"
+  puts "Voici l'état de nos joueurs :"
+  player1.show_state
+  player2.show_state
+  puts " "
+
+  puts "Place à l'attaque !!!!!"
+  player1.attack(player2)
+    if player2.life_points <= 0
+    	break
+    end
+  puts " "
+  player2.attack(player1)  
+ 
+end
+
+#juste pour pimper un peu mon jeu et annoncer le vainqueur <3
+winner = player1.life_points > player2.life_points ? player1 : player2
+
+puts " "
+puts "**********************************************"
+puts "And the winner is : #{winner.name}"
+puts "**********************************************"
+
+#binding.pry
---
+++ lib/player.rb
@@ -0,0 +1,69 @@
+
+
+class Player
+
+  attr_accessor :name, :life_points
+  
+  def initialize(name)
+  	@name = name
+  	@life_points = 10
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} point de vie !!"
+  end
+
+  def get_damage(damage)
+  	@life_points = @life_points - damage #calcul le nombre de vies qui restent au joueur quand attaqué.
+  	#puts "#{@name} a perdu #{damage} points de vie et il lui en reste #{@life_points} !!" 
+
+  	if @life_points <= 0 #vérifie si le joueur est mort ou pas.
+  		puts "#{@name} a été tué..."
+  	end
+  end
+
+  def attack(player)
+    puts "#{@name} attaque #{player.name}"
+    damage = compute_damage #permet de récupérer la valeur determinée au hasard par compute_damage
+    puts "#{@name} inflige #{damage} points de dommage à #{player.name} !!"
+
+    player.get_damage(damage) #appelle la methode precedente pour calculer les points qui restent
+  end
+
+  def compute_damage
+    return rand(1..6) #détermine un nombre de points perdu au hasard
+  end
+
+
+
+end
+
+
+class HumanPlayer < Player
+  
+  attr_accessor :weapon_level
+
+  def initialize(name)
+  	super(name) # il faut le mettre avant sinon, HumanPlayer aura 10 points de vie au lieu de 100
+
+    @life_points = 100
+  	@weapon_level = 1
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} point de vie et une arme de niveau #{@weapon_level}!!"
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+end
+
